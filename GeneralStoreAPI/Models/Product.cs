dab8419bdeefff7dd1ec5226bf3a31a6064d9aa8
Marcus <marcus317@gmail.com>
2021-08-10 21:17:40 -0400

    Add project files.

---
+++ GeneralStoreAPI/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreAPI.Models
+{
+    public class Product
+    {
+        [Key]
+
+        public string Sku { get; set; }
+
+        public string Name { get; set; }
+
+        public double Cost { get; set; }
+
+        public int NumberInInventory { get; set; }
+
+        public bool IsInStock { get; set; }
+    }
+}
